3129ef4a244365acf8417441a7b1c0a9b41d23df
Zouki'Dev <zouki.dev@gmail.com>
2020-02-05 13:30:56 +0100

    Update dashboard info

--- app/Http/Controllers/Home2Controller.php
+++ app/Http/Controllers/Home2Controller.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use App\Salary;
 
 class Home2Controller extends Controller
 {
@@ -13,7 +15,19 @@ class Home2Controller extends Controller
 
     public function index()
     {
-        return view('home2');
+        $salaries = Salary::where('user_id', Auth::user()->id)
+               ->get();
+
+        $earnings_annual = 0;
+        foreach ($salaries as $salary) {
+            // dd(date('Y', strtotime($salary->date)));
+            if (date('Y', strtotime($salary->date)) == date('Y')) {
+                $earnings_annual += $salary->amount;
+            }
+        }
+        return view('home2', array(
+            'earnings_annual' => $earnings_annual
+        ));
     }
 
     public function register()
--- resources/views/home2.blade.php
+++ resources/views/home2.blade.php
@@ -33,7 +33,7 @@
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Earnings (Annual)</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $earnings_annual }} &euro;</div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
